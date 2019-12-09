5a4da3db4e131f5250eb878685c7d52c66237209
Никита Куницкий <uedeck777@live.com>
2019-12-09 05:32:30 +0300

    Рефакторинг

--- WebApplication1/Controllers/API/TimetableControllerApi.cs
+++ WebApplication1/Controllers/API/TimetableControllerApi.cs
@@ -9,7 +9,6 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers.API
 {
-	[Route("api/timtables")]
 	public class TimetableControllerApi : Controller
 	{
 		ApplicationDbContext db;
@@ -19,6 +18,8 @@ namespace WebApplication1.Controllers.API
 			this.db = db;
 		}
 
+		[Route("api/timtables")]
+		[HttpGet]
 		public IEnumerable<Timetable> Get(string speciality, string faculty, string group)
 		{
 			return db.Timetables.Include(p => p.Auditorium)
@@ -28,6 +29,27 @@ namespace WebApplication1.Controllers.API
 				.Include(p => p.Teacher)
 				.Include(p => p.Speciality)
 				.Where(p => p.Speciality.SpecialityAbbreviation == speciality && p.Group.Code==group).ToList();
+		}
+
+		[Route("api/specialities")]
+		[HttpGet]
+		public IEnumerable<Speciality> GetSpeciality()
+		{
+			return db.Specialities.ToList();
+		}
+
+		[Route("api/groups")]
+		[HttpGet]
+		public IEnumerable<Group> GetGroup()
+		{
+			return db.Groups.Include(p => p.Speciality).ToList();
+		}
+
+		[Route("api/faculties")]
+		[HttpGet]
+		public IEnumerable<Faculty> Get()
+		{
+			return db.Faculties.ToList();
 		}
 	}
 }
